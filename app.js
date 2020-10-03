082693d6302c139d339c48585b4d3f8c818c3252
Hong <snuhongfrcs@snu.ac.kr>
2020-10-03 14:10:23 +0900

    20201003pm1410 update

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var index2Router = require('./routes/index2');
-var index3Router = require('./routes/index3');
 
 var app = express();
 
@@ -24,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/index2', index2Router);
-app.use('/index3', index3Router);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
